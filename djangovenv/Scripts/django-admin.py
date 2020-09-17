aea5e3c5f347651b21e84a601e146c34dc10f091
Hannah <hannah.e.wood@hotmail.co.uk>
2020-09-17 12:16:04 +0100

    first commit

---
+++ djangovenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\bunty\desktop\hannahmacstuff\dev\eigentechtest\_solution 2 - plain django\djangovenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
