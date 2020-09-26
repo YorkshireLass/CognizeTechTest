340359164bb05cbc8c66b08f39247c314c35d064
Hannah <hannah.e.wood@hotmail.co.uk>
2020-09-26 18:37:53 +0100

    postgres production ready

--- interestingwords/settings.py
+++ interestingwords/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x)awwbodzee*(waj5n!r2go4k$%%)h=-p5(y)b3if4g8e=eazb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
