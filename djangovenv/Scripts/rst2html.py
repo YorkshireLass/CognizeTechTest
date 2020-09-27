ed6d33bb84ceda8378db086b69809035d85e7e29
Hannah <hannah.e.wood@hotmail.co.uk>
2020-09-27 10:53:47 +0100

    clean master branch

---
+++ djangovenv/Scripts/rst2html.py
@@ -0,0 +1,23 @@
+#!c:\users\bunty\desktop\hannahmacstuff\dev\eigentechtest\_solution 2 - plain django\djangovenv\scripts\python.exe
+
+# $Id: rst2html.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing HTML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates (X)HTML documents from standalone reStructuredText '
+               'sources.  ' + default_description)
+
+publish_cmdline(writer_name='html', description=description)
